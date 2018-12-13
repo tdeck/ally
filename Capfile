797511e09ce384300543fdc82b3fe094aa483ffe
Troy H. Deck <troy.deque@gmail.com>
2018-12-12 22:35:53 -0800

    Committing what I have that doesn't work

--- Capfile
+++ Capfile
@@ -13,7 +13,13 @@ require "capistrano/deploy"
 # install_plugin Capistrano::SCM::Svn
 # or
 require "capistrano/scm/git"
+require 'capistrano/rails'
+require 'capistrano/bundler'
+require 'capistrano/rvm'
+require 'capistrano/puma'
+
 install_plugin Capistrano::SCM::Git
+install_plugin Capistrano::Puma
 
 # Include tasks from other gems included in your Gemfile
 #
--- config/deploy.rb
+++ config/deploy.rb
@@ -26,19 +26,20 @@ set :puma_workers, 0
 # Default value for :linked_files is []
 append :linked_files, "config/master.key"
 
-set :pty,             true
-set :use_sudo,        false
-set :deploy_via,      :remote_cache
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :pty, true
+set :use_sudo, false
+set :deploy_via, :remote_cache
+set :bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_error_log, "#{release_path}/log/puma.access.log"
+set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :puma_init_active_record, true # Change to false when not using ActiveRecord
 
+set :keep_releases, 3 # Save space
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
@@ -49,7 +50,7 @@ namespace :puma do
     end
   end
 
-  before :start, :make_dirs
+  before :starting, :make_dirs
 end
 
 namespace :deploy do

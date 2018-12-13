3878732fd3a94938538a69d746269e79c2110da4
Troy H. Deck <troy.deque@gmail.com>
2018-12-12 22:07:13 -0800

    Add some more deploy config.

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,6 +23,19 @@ set :repo_url, "git@github.com:tdeck/ally.git"
 # Default value for :linked_files is []
 append :linked_files, "config/master.key"
 
+set :pty,             true
+set :use_sudo,        false
+set :deploy_via,      :remote_cache
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 

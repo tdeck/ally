02a036ff51162d3a35e80b2d20b02f482df0b19c
Troy H. Deck <troy.deque@gmail.com>
2018-12-13 23:17:42 -0800

    Add credentials for prod Meetup OAuth client
    Since clients can't have more than one redirect URI, we've got a
    separate one for local development and prod. This just hacks that in
    using regular encrypted credentials.
    
    We will want to revisit once this is released:
    https://github.com/rails/rails/pull/33521

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :meetup,
-    Rails.application.credentials.meetup_oauth[:key],
-    Rails.application.credentials.meetup_oauth[:secret],
+    Rails.application.credentials.dig(:meetup_oauth, Rails.env.to_sym, :key),
+    Rails.application.credentials.dig(:meetup_oauth, Rails.env.to_sym, :secret),
     scope: 'event_management',
   )
-  # TODO scope: parameter
 end

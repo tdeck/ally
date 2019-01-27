5091f0f4c2c647aeadb0cf957db1ab459cd9ac4c
Troy H. Deck <troy.deque@gmail.com>
2019-01-26 17:06:06 -0800

    Basic conversion to picnic css
    Uses a hacky label for the flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,15 @@ module ApplicationHelper
   def meetup_client
     @meetup_client ||= MeetupClient.new(oauth_token)
   end
+
+  def embed_svg(filename, options = {})
+    assets = Rails.application.assets
+    file = assets.find_asset(filename).source.force_encoding("UTF-8")
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css "svg"
+    if options[:class].present?
+      svg["class"] = options[:class]
+    end
+    raw doc
+  end
 end

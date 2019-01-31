c2ab47bed3bbdb67cbf41a94bbbdacd7d5561a0e
Troy H. Deck <troy.deque@gmail.com>
2019-01-30 21:33:15 -0800

    Invoke Trumbowyg after page loads
    Also hide rich textareas until it loads.

--- app/assets/javascripts/newsletters.js
+++ app/assets/javascripts/newsletters.js
@@ -1,2 +1,25 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+window.addEventListener('load', function () {
+  // I have to vendor this file because CloudFlare's CDN refuses to set broad CORS headers
+  // that would permit the SVG file to be loaded on localhost.
+  $.trumbowyg.svgPath = '/trumbowyg.svg';
+
+  $('.richtext').trumbowyg({
+    btns: [
+      ['viewHTML'],
+      ['undo', 'redo'], // Only supported in Blink browsers
+      ['formatting'],
+      ['strong', 'em', 'del'],
+      ['link'],
+      // ['insertImage'],
+      ['unorderedList', 'orderedList'],
+      ['horizontalRule'],
+      ['removeformat'],
+      ['fullscreen'],
+    ],
+    resetCss: true
+  });
+}, false);

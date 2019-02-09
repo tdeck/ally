4c4b2c61bac16b274e5e025ecd12581b35d06d62
Troy H. Deck <troy.deque@gmail.com>
2019-02-09 00:30:02 -0800

    Make image reuploads no-op, add JSON result
    There's no way to change an image title now but the titles are kind of
    an afterthought anyway

--- app/controllers/image_uploads_controller.rb
+++ app/controllers/image_uploads_controller.rb
@@ -8,13 +8,28 @@ class ImageUploadsController < ApplicationController
   def create
     image = params.require(:image)
     image_bytes = image.read
-    @record = ImageUpload.create!(
-      title: params.require(:title),
-      mime_type: image.content_type,
-      bytes: image_bytes,
-      sha1: Digest::SHA1.hexdigest(image_bytes),
-      creator_uid: session['uid']
-    )
+    sha1 = Digest::SHA1.hexdigest(image_bytes)
+
+    # If we already have the image, don't create another record
+    # Note: There's an unlikely race here
+    @record = ImageUpload.find_by_sha1(sha1)
+
+    unless @record
+      @record = ImageUpload.create!(
+        title: params.require(:title),
+        mime_type: image.content_type,
+        bytes: image_bytes,
+        sha1: sha1,
+        creator_uid: session['uid']
+      )
+    end
+
+    respond_to do |format|
+      format.html # Render view
+      format.json do
+        render json: {url: url_for(@record)}
+      end
+    end
   end
 
   def show

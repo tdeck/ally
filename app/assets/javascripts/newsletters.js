2a2aa390c032386a7927f25e20e0df546f3e24b1
Troy H. Deck <troy.deque@gmail.com>
2019-02-08 21:45:24 -0800

    Make Trumbowyg always show
    Also delegates jQuery to jquery-rails

--- app/assets/javascripts/newsletters.js
+++ app/assets/javascripts/newsletters.js
@@ -2,12 +2,11 @@
 // All this logic will automatically be available in application.js.
 
 
-window.addEventListener('load', function () {
-  // I have to vendor this file because CloudFlare's CDN refuses to set broad CORS headers
-  // that would permit the SVG file to be loaded on localhost.
-  $.trumbowyg.svgPath = '/trumbowyg.svg';
-
+document.addEventListener('turbolinks:load', function() {
   $('.richtext').trumbowyg({
+    // I have to vendor this file because CloudFlare's CDN refuses to set broad
+    // CORS headers that would permit the SVG file to be loaded on localhost.
+    svgPath: '/trumbowyg.svg',
     btns: [
       ['viewHTML'],
       ['undo', 'redo'], // Only supported in Blink browsers
@@ -22,4 +21,4 @@ window.addEventListener('load', function () {
     ],
     resetCss: true
   });
-}, false);
+});

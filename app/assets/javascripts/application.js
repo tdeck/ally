2e9de4a73959964923dfb9761d91e786ca554dce
Troy H. Deck <troy.deque@gmail.com>
2019-02-09 21:03:16 -0800

    Hack to make image form values show after back button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,8 +17,7 @@
 //= require turbolinks
 //= require_tree .
 
-// Support Picnic-style image uploads
-document.addEventListener("turbolinks:load", function() {
+document.addEventListener("DOMContentLoaded", function() {
   [].forEach.call(document.querySelectorAll('.dropimage'), function(img){
     img.onchange = function(e){
       var inputfile = this, reader = new FileReader();
@@ -26,6 +25,16 @@ document.addEventListener("turbolinks:load", function() {
         inputfile.style['background-image'] = 'url('+reader.result+')';
       }
       reader.readAsDataURL(e.target.files[0]);
+      console.log("FIle after upload:", e.target.files[0]);
+    }
+  });
+
+  // If the image was populated on page load (happens when someone hits back
+  // to a form they already filled out), then we need to send a synthetic
+  // event to cause it to be displayed by the code above
+  [].forEach.call(document.querySelectorAll('.dropimage input'), function(input){
+    if (input.files.length > 0) {
+      input.dispatchEvent(new Event('change', {bubbles: true}));
     }
   });
 });

27457e575e1f97b929a541cf25a17e2e83e41d34
Troy H. Deck <troy.deque@gmail.com>
2019-02-10 20:41:37 -0800

    Limit settings key length to satisfy MySQL key limit

--- db/migrate/20190211024353_create_settings.rb
+++ db/migrate/20190211024353_create_settings.rb
@@ -1,7 +1,7 @@
 class CreateSettings < ActiveRecord::Migration[5.2]
   def change
     create_table :settings do |t|
-      t.string :key, required: true
+      t.string :key, required: true, limit: 191 # Max index length
       t.string :value
 
       t.timestamps

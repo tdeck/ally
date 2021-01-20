428ce0391e18fb6ea0e270dffe4fd51db9cb73cd
Troy H. Deck <troy.deque@gmail.com>
2021-01-20 00:19:29 -0800

    Bump newsletter MAX_ARTICLES to 5
    Apparently articles are going to be used more. I should set up a proper
    dynamic form when I get time.

--- app/helpers/newsletters_helper.rb
+++ app/helpers/newsletters_helper.rb
@@ -1,6 +1,6 @@
 module NewslettersHelper
   HIDDEN_EVENT_FIELDS = [:id, :month, :mday, :wday]
-  MAX_ARTICLES = 3
+  MAX_ARTICLES = 5
 
   def default_lead_html
     Setting.get_str(SettingsKeys::NEWSLETTER_LEAD_HTML)
